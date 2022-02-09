c999053f0f17bed51c6fe4c410602b19dd4c7247
MiracleSNeko <miracle.neko@qq.com>
2022-02-09 13:25:22 +0800

    Init: 初始化 master 分支

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,9 @@
-// #![windows_subsystem = "windows"]
+#[windows_subsystem = "windows"]
+
 use bevy::prelude::*;
 
-pub(crate) mod cg;
-pub(crate) mod media;
-pub(crate) mod script;
-pub(crate) mod text;
-pub(crate) mod ui;
-
 fn main() {
-    
+    App::new()
+        .add_system(|| println!("Hello, world!"))
+        .run()
 }
--- src/platform/mod.rs
+++
@@ -1,8 +0,0 @@
-pub(crate) use self::platform::*;
-
-#[cfg(target_os = "windows")]
-#[path = "windows/mod.rs"]
-mod platform;
-
-#[cfg(not(target_os = "windows"))]
-compile_error!("Current OS not supported yet.");
