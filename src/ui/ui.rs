4bafb19ec74888096adf231b62ebd7c1a3e98bab
MiracleSNeko <miracle.neko@qq.com>
2022-11-20 14:00:30 +0800

    🐞 fix(UI): 适配 bevy 0.9.0 坐标系的修改
    - 适配 bevy v0.9.0 将 UI 坐标系调整为左手系

--- src/ui/ui.rs
+++ src/ui/ui.rs
@@ -17,26 +17,26 @@ pub const WINDOW_HEIGHT: f32 = 600.0;
 lazy_static! {
     pub static ref MAIN_TITLE_LAYOUT: Vec<Descriptor> = vec![descriptor!(
         MAIN_TITLE_BG_GUID.0,
-        WidgetDescriptor::image(image!((WINDOW_WIDTH, WINDOW_HEIGHT), (0.0, WINDOW_HEIGHT))),
+        WidgetDescriptor::image(image!((WINDOW_WIDTH, WINDOW_HEIGHT), (0.0, 0.0))),
         Some(GroupDescriptor(vec![
             descriptor!(
                 MAIN_TITLE_START_BUTTON_GUID.0,
-                WidgetDescriptor::button(button!((100.0, 40.0), (200.0, WINDOW_HEIGHT - 480.0))),
+                WidgetDescriptor::button(button!((100.0, 40.0), (200.0, 480.0))),
                 None
             ),
             descriptor!(
                 MAIN_TITLE_CONFIG_BUTTON_GUID.0,
-                WidgetDescriptor::button(button!((100.0, 40.0), (300.0, WINDOW_HEIGHT - 480.0))),
+                WidgetDescriptor::button(button!((100.0, 40.0), (300.0, 480.0))),
                 None
             ),
             descriptor!(
                 MAIN_TITLE_EXTRA_BUTTON_GUID.0,
-                WidgetDescriptor::button(button!((100.0, 40.0), (400.0, WINDOW_HEIGHT - 480.0))),
+                WidgetDescriptor::button(button!((100.0, 40.0), (400.0, 480.0))),
                 None
             ),
             descriptor!(
                 MAIN_TITLE_EXIT_BUTTON_GUID.0,
-                WidgetDescriptor::button(button!((100.0, 40.0), (500.0, WINDOW_HEIGHT - 480.0))),
+                WidgetDescriptor::button(button!((100.0, 40.0), (500.0, 480.0))),
                 None
             )
         ]))
@@ -46,11 +46,11 @@ lazy_static! {
 lazy_static! {
     pub static ref START_TITLE_LAYOUT: Vec<Descriptor> = vec![descriptor!(
         START_TITLE_BG_GUID.0,
-        WidgetDescriptor::image(image!((WINDOW_WIDTH, WINDOW_HEIGHT), (0.0, WINDOW_HEIGHT))),
+        WidgetDescriptor::image(image!((WINDOW_WIDTH, WINDOW_HEIGHT), (0.0, 0.0))),
         Some(GroupDescriptor(vec![
             descriptor!(
                 START_TITLE_DIALOG_TEXTBOX_DUMMY_BUTTON_GUID.0,
-                WidgetDescriptor::button(button!((787.0, 142.0), (13.0, WINDOW_HEIGHT - 450.0))),
+                WidgetDescriptor::button(button!((787.0, 142.0), (13.0, 450.0))),
                 None
             ),
         ]))
